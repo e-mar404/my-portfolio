6b0e64151d8d9da1d003169d3102ef464b74657b
e-mar404 <egmarinn@cougarnet.uh.edu>
2022-05-16 13:50:23 -0500

    About page route and Trail components added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
+import About from './pages/About';
+import Trail from './components/Cursor Trail';
 
 function App() {
   return (
@@ -12,8 +14,11 @@ function App() {
         <Routes>
           <Route path='/my-portfolio' element={<Home/>} />
         </Routes>
+        <Routes>
+          <Route path='/about' element={<About/>} />
+        </Routes>
       </BrowserRouter>
-      
+      <Trail />
     </div>
   );
 }
