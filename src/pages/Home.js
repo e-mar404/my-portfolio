02e025a56b838439ffea5f63707a48df85451fe9
e-mar404 <egmarinn@cougarnet.uh.edu>
2022-05-18 11:06:15 -0500

    complete color theme and arangement change

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { NavLink as Link } from "react-router-dom";
 import "./Home.css";
+
 
 const Home = () => {
     return (
-        <div className="intro">
+        <div className="intro-background">
+        <div className="intro-container">
             <h1 className="intro-name">Emilio Marin</h1>
-            <p className="intro-description">I am a student at UH</p>
+            <p className="intro-description">I study <mark><Link className='links' to='/about'>Computer Science</Link></mark> at the University of Houston &</p>
+            <p className="intro-description">currently <mark>work</mark> as a Part-Time <mark>Systems Administrator</mark></p>
+        </div>
         </div>
     );
 };
