61ae96a769395efdc413207385b284cdd518c09f
e-mar404 <egmarinn@cougarnet.uh.edu>
2022-05-18 11:05:45 -0500

    Using in text links insteas of nav bar

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
   return (
     <div className="App">      
       
+      {/*
+      Routs for Navbar, changes URL
+
       <BrowserRouter>
         <Navbar/>
         <Routes>
@@ -19,6 +22,17 @@ function App() {
         </Routes>
       </BrowserRouter>
       <Trail />
+      */}
+
+      {/* New routers that let me use the text as buttons, nav bar wont be used anymore */}
+      <BrowserRouter>
+        <Routes>
+          <Route path='/my-portfolio' element={<Home />} />
+        </Routes>
+        <Routes>
+          <Route path='/about' element={<About />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
