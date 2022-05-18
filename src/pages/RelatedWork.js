5d6ce60b78b869d9a0beea2715fcd3a25a705ce9
e-mar404 <egmarinn@cougarnet.uh.edu>
2022-05-18 16:53:59 -0500

    Created related work page

---
+++ src/pages/RelatedWork.js
@@ -0,0 +1,12 @@
+import './RelatedWork.css';
+import React from "react";
+
+
+const RelatedWork = () => {
+    return (
+        <div className='work-container'>
+            <h1 className='work-text'>Talk about past projects and work relating to what I want to do</h1>
+        </div>
+    );
+}
+export default RelatedWork;
