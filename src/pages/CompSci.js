149c1c6f20e006ce00ead2dc83a99f09698b2293
e-mar404 <egmarinn@cougarnet.uh.edu>
2022-05-18 16:53:02 -0500

    About->CompSci

---
+++ src/pages/CompSci.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './CompSci.css';
+
+const CompSci = () => {
+    return (
+        <div className="compsci-container">
+            <h1 className="compsci-text">Things relating to compsci and what languages/etc I know</h1>
+        </div>
+    );
+};
+
+export default CompSci;
